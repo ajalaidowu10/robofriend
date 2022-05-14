3c8ed34f21e828902bf1b3290d7b6e45c353d9e0
Ajala Idowu <ajalaidowu10@gmail.com>
2022-05-14 19:44:55 +0100

    Add Card component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Card from './components/Card';
 
-ReactDom.render(<h1>Hello Daddy</h1>, document.getElementById('root'));
+ReactDom.render(<Card key="1" id="1" name="Ajala Idowu" email="ajalaidowu10@gmail.com"/>, document.getElementById('root'));
